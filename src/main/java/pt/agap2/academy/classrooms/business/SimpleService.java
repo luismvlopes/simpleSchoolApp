010fdd08920a71b5c117edfb6c1af6457c127441
Luis Lopes <luis.lopes@agap2.pt>
2019-01-14 17:50:53 +0000

    added REST endpoints to find main entities

---
+++ src/main/java/pt/agap2/academy/classrooms/business/SimpleService.java
@@ -0,0 +1,40 @@
+package pt.agap2.academy.classrooms.business;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pt.agap2.academy.classrooms.data.jpa.Classroom;
+import pt.agap2.academy.classrooms.data.jpa.Student;
+import pt.agap2.academy.classrooms.data.jpa.Teacher;
+import pt.agap2.academy.classrooms.dataaccess.jpa.ClassroomDao;
+import pt.agap2.academy.classrooms.dataaccess.jpa.StudentDao;
+import pt.agap2.academy.classrooms.dataaccess.jpa.TeacherDao;
+
+@Service
+public class SimpleService {
+
+	@Autowired
+	private TeacherDao teacherDao;
+
+	@Autowired
+	private StudentDao studentDao;
+	
+	@Autowired
+	private ClassroomDao classroomDao;
+
+	public List<Teacher> findAllTeachers() {
+		return teacherDao.findAll();
+	}
+
+	public List<Student> findAllStudents() {
+		return studentDao.findAll();
+	}
+	
+	public List<Classroom> findAllClassrooms() {
+		return classroomDao.findAll();
+	}
+	
+
+}
